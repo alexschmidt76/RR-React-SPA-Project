af48b62e5ec0ad8db3dcdca3090b6ba3f873b3f7
Alex Schmidt <alex.schmidt767@gmail.com>
2022-11-10 22:24:38 -0600

    titlebar created

---
+++ dm-interface/src/components/TitleBar.js
@@ -0,0 +1,22 @@
+import { Container, Navbar } from 'react-bootstrap';
+import dndLogo from '../dnd-logo.png';
+
+export default function TitleBar() {
+  return (
+    <Navbar bg="dark" variant="dark">
+        <Container>
+            <Navbar.Brand href="#home">
+            <img
+                alt="dnd 5e logo"
+                src={dndLogo}
+                width="30"
+                height="30"
+                style={{marginRight: '10px'}}
+                className="d-inline-block align-top"
+            />
+            Dungeon Master Interface
+            </Navbar.Brand>
+        </Container>
+    </Navbar>
+  );
+}
